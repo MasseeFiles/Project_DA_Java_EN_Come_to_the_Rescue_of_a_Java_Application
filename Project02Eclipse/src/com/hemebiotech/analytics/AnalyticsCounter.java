0158400eb828a97f70bfe72ba26c1771a2839535
TotoLanson <jbmassee@gmail.com>
2022-12-08 20:29:18 +0100

    Refactoring 4 avec passage de la map au fichier .txt

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,36 +5,34 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-
-	private static int newCount = 0;
-
 	public static void main(String []args) throws Exception {
-		// first get input from file symptoms.txt
+		// Lire
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 		Map<String, Integer> mapSortie = new HashMap<String, Integer>();
 
-		//loop to analyse the list provided by Symptoms.txt
+		// Compter
 		while (line != null) {
-			if (mapSortie.containsKey(line)) {            //recherche si l'élément lu est présent dans la map
-				mapSortie.computeIfPresent(line,(key,val) -> val + 1);		//incementation de 1 si l'élémént est présent
-				System.out.println(mapSortie);		//verification : ce qui se passe à chaque boucle
+			if (mapSortie.containsKey(line)) {          					// Recherche si l'élément lu est présent dans la map
+				mapSortie.computeIfPresent(line,(key,val) -> val + 1);		// Incrementation de 1 si l'élémént est présent
+				System.out.println(mapSortie);								// Verification : ce qui se passe à chaque boucle (à supprimer à la fin)
 			}
-			else {										//creation de l'élément dans la map et incrementation de 1
+			else {															// Creation de l'élément dans la map et incrementation de 1 si l'élément n'est pas present
 				mapSortie.put(line, 1);
 			}
 			line = reader.readLine();
 		}
 
+		// Afficher
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("- Headache: " + headCount + "\n");
-		writer.write("- Rash: " + rashCount + "\n");
-		writer.write("- Dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+		for (int i=0; i < mapSortie.size(); i++) {						// Boucle pour parcourir les éléments de la map
+			writer.write(i.getKey () + "occurs" + i.getValue() + "time(s)");    //pb
+			writer.write("\n");				//Passer une ligne
+		}
+		writer.close();							//Fermer le fichier
 	}
 }

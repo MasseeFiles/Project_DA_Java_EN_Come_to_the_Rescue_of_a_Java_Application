b1ae3933639924fad8d9e221acc39cd836784b3b
TotoLanson <jbmassee@gmail.com>
2022-12-08 09:38:39 +0100

    Refactoring 2 avec introduction de la variable map.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,72 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Map;				//pour utiliser Map
+import java.util.HashMap;			//pour utiliser Map
+import java.util.ArrayList;			//pour utiliser List
+import java.util.Collections;		//pour utiliser frequency
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	public static void main(String[] args) throws Exception {
+			/* lancer class LIRE sur symptoms.txt
+				source : symptoms.txt - resultat : listSymptoms
+			puis lancer class COMPTER sur listSymptoms
+				source : listSymptoms - resultat : mapSortie
+			puis lancer class AFFICHER sur mapSortie
+				source : map_sortie - resultat : result.out
+			 */
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		// class Lire
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));	   		// transmission du chemin du fichier à lire à class Bufferedreader
+		List<String> listSymptoms = new ArrayList<String>();
+		// pb : faire lien entre reader et listSymptoms
+		String line = reader.readLine();  					 // method readline lit les lignes une par une
+		line = reader.readLine();	// get another symptom
 	}
+
+	class Compter {
+		private ArrayList<String> listSymptoms = new ArrayList<String>();
+		private Map<String, Integer> mapSortie = new HashMap<String, Integer>();
+		private int headCount = Collections.frequency(listSymptoms, "headache");
+		private int rashCount = Collections.frequency(listSymptoms, "rash");
+		private int pupilCount = Collections.frequency(listSymptoms, "pupils");
+
+		// remplissage des données de la map
+			mapSortie.put ("headache", headCount);
+			mapSortie.put ("rash", rashCount);
+			mapSortie.put ("pupils", pupilCount);
+	}
+
+		/* test class Compter avec boucle for
+			private Map<String, Integer> map_sortie = new HashMap<String, Integer>();
+			private static int headCount = 0;
+			private static int rashCount = 0;
+			private static int pupilCount = 0;
+
+			// pb : recuperation d'un element de la liste (avec son index) pour le mettre dans la map
+
+
+			for (int i = 0; i < listSymptoms.size; i++ ) {
+				if (listSymptoms.get(i).equals("headache")) {
+				headCount++;
+				}
+				else if (listSymptoms.get(i).equals("rash")) {
+				rashCount++;
+				}
+				else if (listSymptoms.get(i).contains("pupils")) {            //Counts all elements related to pupils (constricted or dilated)
+				pupilCount++;
+				}
+			}
+		*/
+
+	//class Afficher : utilisation des method (keyset et values) ou (entryset)
+	FileWriter writer = new FileWriter ("result.out");
+			writer.write("headache: " + headCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + pupilCount + "\n");
+			writer.close();
 }
+}
+
+

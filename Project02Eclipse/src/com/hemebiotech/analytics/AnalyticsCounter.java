31200b2007edf3a0c5ed8d8a939b51ba3aea897a
TotoLanson <jbmassee@gmail.com>
2022-12-08 15:08:13 +0100

    Refactoring 3 avec introduction des methodes "containsKey" et "computeIfPresent".

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,36 +3,38 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AnalyticsCounter {
 	private static int headCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 
+	private static int newCount = 0;
+
 	public static void main(String []args) throws Exception {
 		// first get input from file symptoms.txt
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
+		Map<String, Integer> mapSortie = new HashMap<String, Integer>();
 
 		//loop to analyse the list provided by Symptoms.txt
 		while (line != null) {
-			if (line.equals("headache")) {
-				headCount++;
+			if (mapSortie.containsKey(line)) {            //recherche si l'élément lu est présent dans la map
+				mapSortie.computeIfPresent(line,(key,val) -> val + 1);		//incementation de 1 si l'élémént est présent
+				System.out.println(mapSortie);		//verification : ce qui se passe à chaque boucle
 			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {			//Counts all elements related to pupils (constricted or dilated)
-				pupilCount++;
+			else {										//creation de l'élément dans la map et incrementation de 1
+				mapSortie.put(line, 1);
 			}
 			line = reader.readLine();
 		}
 
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("- Headache: " + headCount + "\n");
+		writer.write("- Rash: " + rashCount + "\n");
+		writer.write("- Dialated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
 }
-

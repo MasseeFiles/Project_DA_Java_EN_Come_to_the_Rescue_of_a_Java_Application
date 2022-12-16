6242c3d3512a6be9ec2e504b45b2c78e97b70d08
TotoLanson <jbmassee@gmail.com>
2022-12-16 09:24:32 +0100

    Refactoring 6 : récuperation du code fonctionnel developpé en session de mentorat

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,45 +5,34 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.*;
 
 public class AnalyticsCounter {
-	public static void main(String []args) throws Exception {
-		// Lire
-		TestAppel();
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-		Map<String, Integer> mapSortie = new HashMap<String, Integer>();
+	public static void main(String[] args) throws Exception {
+		// Lire + Créer Map
 
-		// Compter
+		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+		String line = reader.readLine();
+		Map<String, Integer> mapSortie = new HashMap<>();
+
 		while (line != null) {
-			if (mapSortie.containsKey(line)) {          					// Recherche si l'élément lu est présent dans la map
-				mapSortie.computeIfPresent(line,(key,val) -> val + 1);		// Incrementation de 1 si l'élémént est présent
-				System.out.println(mapSortie);								// Verification : ce qui se passe à chaque boucle (à supprimer à la fin)
-			}
-			else {															// Creation de l'élément dans la map et incrementation de 1 si l'élément n'est pas present
+			if (mapSortie.containsKey(line)) {                            // Recherche si l'élément lu est présent dans la map
+				mapSortie.computeIfPresent(line, (key, val) -> val + 1);        // Incrementation de 1 si l'élémént est présent
+				System.out.println(mapSortie);                                // Verification : ce qui se passe à chaque boucle (à supprimer à la fin)
+			} else {                                                            // Creation de l'élément dans la map et incrementation de 1 si l'élément n'est pas present
 				mapSortie.put(line, 1);
 			}
 			line = reader.readLine();
 		}
 
 		// Afficher
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter("result.out");
 
-		for (int i=0; i < mapSortie.size(); i++) {						// Boucle pour parcourir les éléments de la map
-			writer.write(i.getKey () + "occurs" + i.getValue() + "time(s)");    //pb
-			writer.write("\n");				//Passer une ligne
+		for (String key : mapSortie.keySet()) {
+			Integer valeur = mapSortie.get(key);
+			writer.write(key);
+			writer.write(" ");
+			writer.write(valeur + "\n");
 		}
-		writer.close();							//Fermer le fichier
-	}
-
-	class TestAppel{
-		int a = 2;
-		int b = 1;
-		int sum = 0;
-		public static TestAppel () {
-			sum = a + b;
-			return sum;
-		}
+		writer.close();                            //Fermer le fichier
 	}
 }

5f3b052dac3cfa73e8eab19bb8493b02af60900e
TotoLanson <jbmassee@gmail.com>
2022-12-19 21:34:31 +0100

    Refactoring 6 avec commentaire exhaustif du code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,7 +8,8 @@ import java.util.Map;
 
 public class AnalyticsCounter {
 	public static void main(String[] args) throws Exception {
-		// Lire + Créer Map
+		// Lire map avec appel de objet ReadSymptomDataFromFile
+
 
 		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
 		String line = reader.readLine();
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -6,37 +6,39 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+// Le but de ReadSymptomDataFromFile est de creer une list de string à partir du fichier indiqué par filepath
+
 /**
  * Simple brute force implementation
  *
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
+	private String filepath;									//filepath indique juste un chemin d'acces
 	
 	/**
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
-	
-	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
+	public ReadSymptomDataFromFile (String filepath) {			//constructeur avec filepath en parametre
+		this.filepath = filepath;								//this.filepath fait reference à l'attribut de l'objet à créer (objet ReadSymptomDataFromFile)
+	}															// ... et filepath fait reference au parametre du constructeur
+
+	@Override													//précise pour la class ReadSymptomDataFromFile le comportement de getsymptoms
+	public List<String> GetSymptoms() {							//List<String> correspond a la valeur de retour de la method GetSymptoms
+		ArrayList<String> result = new ArrayList<>();		//Appel de constructeur pour creer l'objet result (arraylist de string)
 		
 		if (filepath != null) {
-			try {
+			try {												//gestion des exceptions - cas normal
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
 				String line = reader.readLine();
 				
-				while (line != null) {
-					result.add(line);
+				while (line != null) {							//boucle pour parcourir le fichier de symptoms
+					result.add(line);							//ajout de nouvel element dans result
 					line = reader.readLine();
 				}
 				reader.close();
-			} catch (IOException e) {
+			} catch (IOException e) {							//gestion des exceptions - code a executer si erreur
 				e.printStackTrace();
 			}
 		}
